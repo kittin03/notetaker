90b01ca9d830195d098d27a6c732af33838c357b
kittin03 <katarinam@centrum.cz>
2016-10-01 23:37:12 +0200

    adding Main, removing App from create-react-app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Main from './components/Main';
 
 ReactDOM.render(
-  <App />,
+  <Main />,
   document.getElementById('root')
 );
