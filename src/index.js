460394de4fea973ccc17a64f1a68952356a3d4da
kittin03 <katarinam@centrum.cz>
2016-10-02 03:45:43 +0200

    added router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Router, IndexRoute, hashHistory } from 'react-router';
+import Home from './components/Home';
 import Main from './components/Main';
 
 ReactDOM.render(
-  <Main />,
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
