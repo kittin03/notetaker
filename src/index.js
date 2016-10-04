8a3ccffbf624ff4b237424fd86f8900094d1c64f
kittin03 <katarinam@centrum.cz>
2016-10-04 04:03:41 +0200

    temporary production fix (replacing browserHistory with hashHistory)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Router, IndexRoute, browserHistory } from 'react-router';
+import { Route, Router, IndexRoute, hashHistory } from 'react-router';
 import Home from './components/Home';
 import Main from './components/Main';
 import Profile from './components/Profile';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
   {/* browserHistory vs hashHistory - which one? */}
     <Route path="/" component={Main}>
       <Route path="profile/:username" component={Profile} />
